b775444178b11344bee1e80686b21530ad2f0f0b
Taesoon Park <taesoon.park@9folders.com>
2023-08-28 16:42:33 +0900

    KMM SPM package release for 0.1.2

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/KMMBridgeKickStart/co/touchlab/kmmbridgekickstart/allshared-kmmbridge/0.13.0/allshared-kmmbridge-0.13.0.zip"
-let remoteKotlinChecksum = "fe57822fa8ae5806e791558b1a632c3cd3af6f185d8e866ff4e132f40f68a6d4"
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/9folders-taesoon/KMMLib/org/taesoon2/allshared-kmmbridge/0.1.2/allshared-kmmbridge-0.1.2.zip"
+let remoteKotlinChecksum = "7c607673a2f217978c46a550cb498c860a287f8a0ec62887f9474bdd6d3560d8"
 let packageName = "allshared"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
